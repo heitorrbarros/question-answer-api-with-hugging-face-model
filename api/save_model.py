30e2d7030ca0dd751696eb2cbd0917c1432f2f95
heitorrbarros@gmail.com <heitorrbarros@gmail.com>
2023-06-16 11:51:43 -0300

    chore: Remove comments

--- api/save_model.py
+++ api/save_model.py
@@ -8,11 +8,3 @@ model = AutoModelForQuestionAnswering.from_pretrained(model_name)
 nlp = pipeline('question-answering', model=model, tokenizer=tokenizer)
 res = nlp({'question': 'Where do I live?', 'context': 'My name is Wolfgang and I live in Berlin'})
 print(res)
-# #%% load model from local directory if it works
-# model = TFAutoModelForSequenceClassification.from_pretrained(model_path, local_files_only=True)
-# print("-----------  model loaded from local dir ------------")
-# tokenizer = AutoTokenizer.from_pretrained(model_path, local_files_only=True)
-# print("-----------  tokenizer loaded from local dir ------------")
-# classifier = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
-
-# classifier(["good"]) 
